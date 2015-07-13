ba9d2252fb26a817258c612e6b142f2de9c8b53f
dieulinh <linhvn09@gmail.com>
2015-07-13 20:25:54 +0700

    refactor test for index in controller

--- spec/models/dishes_controller_spec.rb
+++ spec/models/dishes_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe DishesController, type: :controller do
 	describe "#index" do
 		let!(:created_dishes) { FactoryGirl.create_list(:dish, 2) }
-		let(:available_dishes) { assigns(:dishes) }
+		
 		it "public user can list all dishes" do
 			get :index
-			expect(available_dishes).to eq (created_dishes)
+			expect(assigns(:dishes)).to eq (created_dishes)
 		end
 	end
 end
