d46174896672985e4283baca75e2ba15ce5ceaea
dieulinh <linhvn09@gmail.com>
2015-07-14 11:56:22 +0700

    sample data for dishes and style the index page of dishes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Dish.create(title: "Asparagus, black olive, goats curd, crispy seeds and nuts", description: 'a cheesecake with a difference from Gordon Ramsaya dessert recipe from Kim Woodward, Head Chef at York & Albany.',
-	cost: 12.29, pax: 2)
+	cost: 12.29, pax: 5)
 Dish.create(title: "Asparagus, black olive, goats curd, crispy seeds and nuts", description: 'a cheesecake with a difference from Gordon Ramsaya dessert recipe from Kim Woodward, Head Chef at York & Albany.',
-	cost: 12.29, pax: 2)
+	cost: 8.25, pax: 2, vegetarian: true)
 Dish.create(title: "Asparagus, black olive, goats curd, crispy seeds and nuts", description: 'a cheesecake with a difference from Gordon Ramsaya dessert recipe from Kim Woodward, Head Chef at York & Albany.',
-	cost: 12.29, pax: 2)
+	cost: 12.29, pax: 3, vegetarian: true)
 Dish.create(title: "Asparagus, black olive, goats curd, crispy seeds and nuts", description: 'a cheesecake with a difference from Gordon Ramsaya dessert recipe from Kim Woodward, Head Chef at York & Albany.',
-	cost: 12.29, pax: 2)
+	cost: 10.59, pax: 1)
 Dish.create(title: "Asparagus, black olive, goats curd, crispy seeds and nuts", description: 'a cheesecake with a difference from Gordon Ramsaya dessert recipe from Kim Woodward, Head Chef at York & Albany.',
-	cost: 12.29, pax: 2)
+	cost: 15.29, pax: 2)
 Dish.create(title: "Asparagus, black olive, goats curd, crispy seeds and nuts", description: 'a cheesecake with a difference from Gordon Ramsaya dessert recipe from Kim Woodward, Head Chef at York & Albany.',
-	cost: 12.29, pax: 2)
+	cost: 34.89, pax: 6)
