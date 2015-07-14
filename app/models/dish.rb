dc7a52abf1843bdf31bb66f15e688eef32352f55
dieulinh <linhvn09@gmail.com>
2015-07-14 09:22:09 +0700

    modify dish model with rspec

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,6 @@
 class Dish < ActiveRecord::Base
 	validates :title, presence: true
+	validates_numericality_of :pax, only_integer: true
+	validates_numericality_of :cost, greater_than: 0
+
 end
---
+++ spec/models/dish_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Dish, type: :model do
+	describe "It should have valid data as a dish" do
+		let!(:dish) { FactoryGirl.create(:dish) }
+		
+		it { shoud validate_presence_of :title }
+		it { should validate_presence_of :description }
+		it { should validate_numericality_of(:cost).is_greater_than(0) } 
+		it { should validate_numericality_of(:pax).only_integer }
+
+	end
+
+	
+end
